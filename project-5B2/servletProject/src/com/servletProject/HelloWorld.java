f07a11952ef0ef36c9d716af2cf185af6173c18a
msg-1 <maheshshivagoud@gmail.com>
2024-07-29 11:36:09 +0530

    servlet- basic

---
+++ project-5B2/servletProject/src/com/servletProject/HelloWorld.java
@@ -0,0 +1,60 @@
+package com.servletProject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		//step:1 inform browser the context type
+		response.setContentType("text/html");
+		
+		
+		//2. get the printWriter which allows us to send content to the browser.
+	PrintWriter	out= response.getWriter();
+	
+	
+	//3.  the content to render as html.
+		out.println("<html><body>");
+		out.println("<h1>Hello world</h1>");
+		out.println("</body></html>");
+	
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ project-5B2/servletProject/src/com/servletProject/TestParamServlet.java
@@ -0,0 +1,70 @@
+package com.servletProject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestParamServlet
+ */
+@WebServlet("/TestParamServlet")
+public class TestParamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestParamServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		//step 1
+		
+		response.setContentType("text/html");
+		
+		//step-2
+		
+		PrintWriter out= response.getWriter();
+		
+		//step-3 configuration params from .xml file
+		
+	ServletContext	context= getServletContext();
+	
+	String teamName= context.getInitParameter("Project-team-name");
+	
+	
+	//step-4
+	
+	out.println("<html><body>");
+	
+	out.println("<h1>this is the team name:</h1>" + teamName);
+	
+	out.println("</body></html>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
